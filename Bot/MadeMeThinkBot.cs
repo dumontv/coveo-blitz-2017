8f8f915f072d902dca0fe703c52d21e3b6c6fd67
Anthony Bouchard <boucharda2497@gmail.com>
2017-01-07 09:53:35 -0500

    first mademethinkbot scheme

--- Bot/MadeMeThinkBot.cs
+++ Bot/MadeMeThinkBot.cs
@@ -7,15 +7,56 @@ namespace Coveo.Bot
     {
         private StateMachine _botState;
 
+        private int _currentCustomerId;
+
         public string Move(GameState state)
         {
-            return _botState.Act(state);
+            Customer myCustomer = state.customers[_currentCustomerId];
+            if (_botState.GetType() == typeof(ChasingState) && 
+                state.myHero.life <= 30) 
+            {
+                _botState = new HealingState();
+            }
             
+            if (_botState.GetType() == typeof(ChasingState) &&
+                state.myHero.burgerCount == myCustomer.burger &&
+                state.myHero.frenchFriesCount == myCustomer.frenchFries) 
+            {
+                _botState = new DeliveringState();
+            }
+
+            Pos myPos = state.myHero.pos;
+            Tile[] _adjacentTiles = new Tile[4];
+            _adjacentTiles[0] = state.board[myPos.x][myPos.y - 1]; //Top
+            _adjacentTiles[1] = state.board[myPos.x - 1][myPos.y]; //Left
+            _adjacentTiles[2] = state.board[myPos.x][myPos.y + 1]; //Bottom
+            _adjacentTiles[3] = state.board[myPos.x + 1][myPos.y]; //Right
+
+            for (int i = 0; i < _adjacentTiles.Length; i++) {
+                if (_adjacentTiles[i] == Tile.CUSTOMER_1)
+                {
+                    
+                }
+                else if (_adjacentTiles[i] == Tile.CUSTOMER_2) 
+                {
+
+                }
+                else if (_adjacentTiles[i] == Tile.CUSTOMER_3) 
+                {
+
+                }
+                else if (_adjacentTiles[i] == Tile.CUSTOMER_4)
+                {
+
+                }
+            }
+
+            return _botState.Act(state);   
         }
 
         public void Setup()
         {
-            throw new NotImplementedException();
+            _botState = new ChasingState();
         }
 
         public void Shutdown()
